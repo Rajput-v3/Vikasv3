17b63dfc5a76ee93e0087e5fd6a3b5035d3bcc85
Vikas Rajput <rajputhere173@gmail.com>
2025-03-13 12:40:16 +0530

    Update imgur.js

--- Priyansh/commands/imgur.js
+++ Priyansh/commands/imgur.js
@@ -1,22 +1,32 @@
 module.exports.config = {
-    name: "imgur",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "Aadi Gupta",
-    description: "",
-    commandCategory: "Game",
-    usages: "[reply]",
-    cooldowns: 5,
-    dependencies: {
-      "axios": ""
-    }
+  name: "imgur",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "NAYAN",// cmd convert to MR.AYAN **//
+  description: "Create Image&video link",
+  commandCategory: "Image",
+  cooldowns: 1,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
 };
 
-module.exports.run = async ({ api, event }) => {
-const axios = global.nodemodule['axios'];  
-var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-    if(!linkanh) return api.sendMessage('Please reply or enter a link 1 image!!!', event.threadID, event.messageID)
-const res = await axios.get(`https://imgur-api-by-koja.xx0xkoja.repl.co/imgur?link=${encodeURIComponent(linkanh)}`);    
-var img = res.data.uploaded.image;
-    return api.sendMessage(`${img}`, event.threadID, event.messageID);
-                                        }
+
+module.exports.run = async ({ api, event, args }) => {
+    const axios = global.nodemodule['axios'];
+  const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api
+    const linkanh = event.messageReply.attachments[0].url || args.join(" ");
+    if (!linkanh)
+        return api.sendMessage('[âšœï¸�]âžœ Please give feedback or enter the image or vide link', event.threadID, event.messageID);
+    try {
+      var tpk = `",`;
+        const allPromise = (await Promise.all(event.messageReply.attachments.map(item => axios.get(`${n}/imgurv2?link=${encodeURIComponent(item.url)}`)))).map(item => item.data.uploaded.image);
+        return api.sendMessage(`\n"` + allPromise.join('"\n"') + tpk, event.threadID, event.messageID);
+    }
+    catch (e) {
+        return api.sendMessage('[âšœï¸�]âžœ An error occurred while executing the command', event.threadID, event.messageID);
+    }
+};
